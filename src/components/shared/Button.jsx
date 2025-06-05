582f87964d499931249a67c881193e636c9efd9f
dilawerhussain-dh <dh@imangu.com>
2025-06-05 10:54:45 +0500

    initial commit

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,23 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import PropTypes from "prop-types";
+import React from "react";
+
+function Button({
+  children,
+  version = "primary",
+  type = "button",
+  isDisabled = false,
+}) {
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
+}
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  version: PropTypes.string,
+  type: PropTypes.string,
+  isDisabled: PropTypes.bool,
+};
+export default Button;
